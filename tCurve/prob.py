b358187a488b712431538fdea64774df346c8153
wholt <captain.cholt@gmail.com>
2020-09-15 13:51:41 -0500

    Red Light |

--- tCurve/prob.py
+++ tCurve/prob.py
@@ -125,9 +125,9 @@ def _integrate(t, n, _f):
         x = s-1
         while(count < x):
             if(count % 2 == 0):
-                simpsonNew += 4*_f(lowBound + (multi * w), n) 
+                simpsonNew += 4*_f(lowBound + (multi*w), n) 
             else:
-                simpsonNew += 2*_f(lowBound + (multi * w), n)
+                simpsonNew += 2*_f(lowBound + (multi*w), n)
             multi += 1
             count += 1
         simpsonNew += _f(highBound,n)
