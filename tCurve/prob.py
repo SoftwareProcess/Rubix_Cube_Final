8e715c7a82a77361b6c9ebca547feb13791908a6
wholt <captain.cholt@gmail.com>
2020-09-15 16:16:01 -0500

    Red Light |

--- tCurve/prob.py
+++ tCurve/prob.py
@@ -113,16 +113,19 @@ def _integrate(t, n, _f):
     simpsonOld = 0.0
     simpsonNew = epsilon
     s = 8.0
-    #while (abs((simpsonNew - simpsonOld) / simpsonNew) > epsilon):
-    #simpsonOld = simpsonNew
-    w=(t-lowBound)/s # w = (highBound - lowBound) / s
-    #simpsonNew = (w/3) * (_f(lowBound,n) + 4*_f(lowBound + w, n) + 2*_f(lowBound + 2*w, n)
-    #                    + 4*_f(lowBound+3*w, n) + _f(lowBound+4*w, n)) 
-    
-    simpsonNew = (w/3) * (_f(lowBound,n) + 4*_f(lowBound + w, n) + 2*_f(lowBound + 2*w, n)
+    while (abs((simpsonNew - simpsonOld) / simpsonNew) > epsilon):
+        simpsonOld = simpsonNew
+        if (s == 4):
+            w=(t-lowBound)/s # w = (highBound - lowBound) / s
+            simpsonNew = (w/3) * (_f(lowBound,n) + 4*_f(lowBound + w, n) + 2*_f(lowBound + 2*w, n)
+                                  + 4*_f(lowBound+3*w, n) + _f(lowBound+4*w, n)) 
+        if (s == 8):
+            simpsonNew = (w/3) * (_f(lowBound,n) + 4*_f(lowBound + w, n) + 2*_f(lowBound + 2*w, n)
                         + 4*_f(lowBound+3*w, n) + 2*_f(lowBound + 4*w, n) + 4*_f(lowBound + 5*w, n) 
                         + 2*_f(lowBound + 6*w, n) + 4*_f(lowBound + 7*w, n)
-                        + _f(lowBound+ 8*w, n))     
+                        + _f(lowBound+ 8*w, n))
+        else:
+            simpsonOld = 1     
 #         simpsonNew = (w/3) * (_f(lowBound,n))
 #         count = 2
 #         multi = 1
@@ -135,6 +138,6 @@ def _integrate(t, n, _f):
 #             multi += 1
 #             count += 1
 #         simpsonNew += _f(lowBound+(multi*w),n)
-#         s = s * 2
+        s = s * 2
     return simpsonNew
     
