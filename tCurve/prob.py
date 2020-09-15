ac5fae48d153809e03e3ee67a1f24506ca14d193
wholt <captain.cholt@gmail.com>
2020-09-15 15:47:55 -0500

    Red Light |

--- tCurve/prob.py
+++ tCurve/prob.py
@@ -114,24 +114,23 @@ def _integrate(t, n, _f):
     simpsonNew = epsilon
     s = 4.0
     #while (abs((simpsonNew - simpsonOld) / simpsonNew) > epsilon):
-    while (abs((simpsonNew - simpsonOld) / simpsonNew) == 1):
-        simpsonOld = simpsonNew
-        w=(t-lowBound)/s # w = (highBound - lowBound) / s
-#         simpsonNew = (w/3) * (_f(lowBound,n) + 4*_f(lowBound + w, n) + 2*_f(lowBound + 2*w, n)
-#                         + 4*_f(lowBound+3*w, n) + _f(lowBound+4*w, n)) 
+    #simpsonOld = simpsonNew
+    w=(t-lowBound)/s # w = (highBound - lowBound) / s
+    simpsonNew = (w/3) * (_f(lowBound,n) + 4*_f(lowBound + w, n) + 2*_f(lowBound + 2*w, n)
+                        + 4*_f(lowBound+3*w, n) + _f(lowBound+4*w, n)) 
         
-        simpsonNew = (w/3) * (_f(lowBound,n))
-        count = 2
-        multi = 1
-        x = s-1
-        while(count <= s):
-            if(count % 2 == 0):
-                simpsonNew += 4*_f(lowBound + (multi*w), n) 
-            else:
-                simpsonNew += 2*_f(lowBound + (multi*w), n)
-            multi += 1
-            count += 1
-        simpsonNew += _f(lowBound+(multi*w),n)
-        s = s * 2
-    return simpsonNew
+#         simpsonNew = (w/3) * (_f(lowBound,n))
+#         count = 2
+#         multi = 1
+#         x = s-1
+#         while(count <= s):
+#             if(count % 2 == 0):
+#                 simpsonNew += 4*_f(lowBound + (multi*w), n) 
+#             else:
+#                 simpsonNew += 2*_f(lowBound + (multi*w), n)
+#             multi += 1
+#             count += 1
+#         simpsonNew += _f(lowBound+(multi*w),n)
+#         s = s * 2
+    return lowBound
     
