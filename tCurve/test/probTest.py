a85431e969964c1291433ccf71725bb623da1d7d
wholt <captain.cholt@gmail.com>
2020-09-15 16:52:58 -0500

    Red Light |

--- tCurve/test/probTest.py
+++ tCurve/test/probTest.py
@@ -221,7 +221,7 @@ class ProbTest(TestCase):
         self.setN(7)
         self.setTails(2)
         expectedResult = 1.035
-        result = _integrate(self.T, self.N, _f)
+        result = _integrate(self.inputDictionary["t"], self.inputDictionary["n"], _f)
         self.assertAlmostEqual(expectedResult, result, 3)   
     
 #     def test200_210ShouldIntegrateNominalCase2Tail(self):
