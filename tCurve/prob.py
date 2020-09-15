0cadaf31de298984d3b71fcb922573a9ef450f2f
wholt <captain.cholt@gmail.com>
2020-09-15 12:38:04 -0500

    Red Light |

--- tCurve/prob.py
+++ tCurve/prob.py
@@ -121,11 +121,10 @@ def _integrate(t, n, _f):
        #Make a different variable to keep track of multiplications
         #simpsonNew = (w/3) * (_f(lowBound,n) + 4*_f(lowBound + w, n) + 2*_f(lowBound + 2*w, n)
                    #     + 4*_f(lowBound+3*w, n) + _f(lowBound+4*w, n)) 
-        x = 4
         multi = 0
         simpsonNew = (w/3) * (_f(lowBound,n))
         count = 2
-        while(count < x):
+        while(count < s):
             if(count % 2 == 0):
                 simpsonNew += 4*_f(lowBound + (multi * w), n) 
             else:
