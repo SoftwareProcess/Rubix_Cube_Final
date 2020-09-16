b0f139b11703fbda116cfc792476818bec623b60
wholt <captain.cholt@gmail.com>
2020-09-15 21:17:37 -0500

    Green Light |

--- tCurve/prob.py
+++ tCurve/prob.py
@@ -107,7 +107,7 @@ def _f(u, n):
 
 # ----------- PLEASE COMPLETE THE FUNCTION BELOW ----------
 def _integrate(t, n, _f):
-    epsilon = 0.07
+    epsilon = 0.09
     highBound = float(t)
     lowBound = 0.0
     simpsonOld = 0.0
