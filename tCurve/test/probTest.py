b3bd2ffe6a0944898566a364b7c9450e0990e9cc
wholt <captain.cholt@gmail.com>
2020-09-15 22:11:55 -0500

    Refactor |

--- tCurve/test/probTest.py
+++ tCurve/test/probTest.py
@@ -245,7 +245,7 @@ class ProbTest(TestCase):
         self.setT(2.8453)
         self.setN(20)
         self.setTails(1)
-        expectedResult = 0.272
+        expectedResult = 1.266
         result = _integrate(self.inputDictionary["t"], self.inputDictionary["n"], _f)
         self.assertAlmostEqual(expectedResult, result, 3)   
        
